d39e08967b9f1d15a7417981f9b52ac243d4d8dd
hpdhillon <57342519+hpdhillon@users.noreply.github.com>
2021-01-16 13:03:59 -0500

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -21,10 +21,11 @@ def main():
 
     with st.spinner(f"Loading {selection} ..."):
         ast.shared.components.write_page(page)
-    st.sidebar.title("Contribute")
+    st.sidebar.title("FAQ")
     st.sidebar.info(
-        "This an open source project and you are very welcome to **contribute** your awesome "
-        "comments, questions, resources and apps to me @ harpal.dhillon@rutgers.edu"
+        "The model requires at least 4 sentences to be accurate. Current accuracy is 86 percent, so that means it's possible for it to mess up sometimes!"
+        "Reporting mistakes greatly helps us predict better in the future. As of right now, we do not have access to any user data (we'd have to guess your usernames for that)."
+        "So if you'd like to help us, please fill out this google form with your journal entry and what you feel the model should have output. Thank you!"
     )
     st.sidebar.title("About")
     st.sidebar.info(
